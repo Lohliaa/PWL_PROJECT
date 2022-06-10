90faf1dc4e7ea2264fca1459109c025ccd8f27e7
= <LiaAufarrahman09@gmail.com>
2022-06-10 09:58:09 +0700

    Update Sidebar dan header

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -42,7 +42,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link " href="customer">
+          <a class="nav-link " href="..">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
             </div>
@@ -50,7 +50,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link " href="customer">
+          <a class="nav-link " href="..">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
             </div>
